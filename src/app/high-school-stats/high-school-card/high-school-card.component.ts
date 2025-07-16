b82415e13f31d99f41eb16a6e036c9ddfd610f4b
TeoB2003 <bolota.teofil2003@gmail.com>
2025-07-16 15:37:15 +0300

    75%

---
+++ src/app/high-school-stats/high-school-card/high-school-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-high-school-card',
+  imports: [],
+  templateUrl: './high-school-card.component.html',
+  styleUrl: './high-school-card.component.scss'
+})
+export class HighSchoolCardComponent {
+
+}
--- src/app/high-school-stats/high-school-stats.component.ts
+++ src/app/high-school-stats/high-school-stats.component.ts
@@ -1,18 +1,63 @@
-import { Component } from '@angular/core';
-import {CountySelectorComponent} from '../shared/components/county-selector/county-selector.component'
+import { Component, inject } from '@angular/core';
+import { CountySelectorComponent } from '../shared/components/county-selector/county-selector.component'
 import { County } from '../shared/county.enum';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-high-school-stats',
-  imports: [CountySelectorComponent],
+  imports: [CountySelectorComponent, FormsModule],
   templateUrl: './high-school-stats.component.html',
   styleUrl: './high-school-stats.component.scss'
 })
 export class HighSchoolStatsComponent {
   selectedCounty: County | '' = '';
+  router = inject(Router)
+  route = inject(ActivatedRoute);
 
-  onCountySelected(county: string)
+  showFilterModal = false;
+  showSortModal = false;
+
+
+  filters = {
+    profil: [] as string[],
+    minMean: 0,
+    minPromotionPercent: 0
+  };
+  availableProfiles = ['Matematică-Informatică', 'Științe ale Naturii', 'Uman', 'Tehnologic', 'Vocational'];
+
+  sortCriteria='BAC'
+
+  ngOnInit(): void {
+    const countyParam = this.route.snapshot.paramMap.get('county');
+    if (countyParam && Object.values(County).includes(countyParam as County)) {
+      this.selectedCounty = countyParam as County;
+    }
+  }
+
+  onCountySelected(county: string) {
+    this.selectedCounty = county as County;
+    this.router.navigate([this.router.url, county]);
+  }
+
+  applySort(sortType: string) {
+    this.sortCriteria=sortType
+  }
+
+  applyFilters() {
+    this.showFilterModal=false
+  }
+
+  showSort() {
+    this.showSortModal = true;
+  }
+
+  showFilter() {
+    this.showFilterModal = true;
+  }
+
+  resetFilters()
   {
-     this.selectedCounty = county as County;
+
   }
 }
